c779616ae2f5c9b1ac98da599c2d7840464dea14
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2021-05-04 14:49:14 +0100

    Started tidying up auxiliary defs for id-type structure

--- TypeTheory/Initiality/SplitTypeCat_ExtraStructure.v
+++ TypeTheory/Initiality/SplitTypeCat_ExtraStructure.v
@@ -8,105 +8,11 @@ Require Import TypeTheory.ALV1.TypeCat.
 Require Import TypeTheory.Initiality.SplitTypeCat_General.
 Require Import TypeTheory.Initiality.SplitTypeCat_Structure.
 
-Section Pi_eta_structure.
+Require Import TypeTheory.Initiality.SplitTypeCat_Maps. (* TODO: check what this was needed for, and upstream it *)
 
-  Context (C : split_typecat) (Π : pi_struct C).
+Section Auxiliary.
 
-  Definition Pi_eta : UU.
-  Proof.
-    refine (forall (Γ : C) (A : C Γ) (B : C (Γ ◂ A)) (p : tm (Π _ A B)),
-               p = _).
-    (* now we have to construct the eta-expansion of [p],
-     which in object-theory pseudocode is [lam x (app p x)] *)
-    refine (pi_intro _ _ _ _ _). (* [pi_intro] is lambda-abstraction *)
-    Fail refine (pi_app _ _ _ _ _ _).
-    (* fails, but informatively:
-       [pi_app] gives a term in a reindexing of B, not B itself.
-    So we need to add a a [transportf].
-    (In the object theory, the type-coercion rule). *)
-    simple refine (transportf _ _ (pi_app Π _ _ _ _ _)).
-    (* In the object theory, the application we want is [ app p x ].
-    But note that this [p] is weakened from [Γ] to [Γ ◂ A],
-    so we need to give that weakening explicitly,
-    i.e. reindexing [p] along the rependent projection from [Γ ◂ A]. *)
-    4: {
-      Fail refine (reind_tm (dpr_typecat A) p).
-    (* informative again: the reindexed pi-type is not definitionally a pi-type *)
-      refine (transportf _ _ (reind_tm (dpr_typecat A) p)).
-      apply pi_form_struct_natural.
-    }
-    (* now supply the argument: the “new variable” / generic term *)
-    2: { apply var_typecat. }
-    (* now some algebra to justify this type-reindexing equality *)
-    eapply pathscomp0. { apply pathsinv0, reind_comp_type_typecat. }
-    eapply pathscomp0. 2: { apply reind_id_type_typecat. }
-    apply maponpaths. 
-    (* look at the definition of the generic term, as a map into a pullback *)
-    unfold var_typecat. apply Auxiliary.Pb_map_commutes_2.
-  Defined.
-
-End Pi_eta_structure.
-
-Section Identity_types.
-  (** The structure to model identity types in a split type-category. *)
-
-  Context (C : split_typecat).
-
-  Definition id_form_struct : UU
-  := ∑ (Id : forall (Γ : C) (A : C Γ) (a b : tm A), C Γ),
-       (forall (Γ Γ' : C) (f : Γ' --> Γ) (A : C Γ) (a b : tm A),
-         (Id Γ A a b) ⦃ f ⦄ = Id Γ' (A⦃f⦄) (reind_tm f a) (reind_tm f b)).
-
-  Definition id_form_struct_pr1 (Id : id_form_struct) := pr1 Id.
-  Coercion id_form_struct_pr1 : id_form_struct >-> Funclass.
-
-  Definition id_form_struct_natural {Id : id_form_struct}
-      {Γ Γ'} (f : Γ' --> Γ) {A} a b
-    : (Id _ _ a b) ⦃ f ⦄ = Id Γ' _ _ _
-  := pr2 Id _ _ f A a b.
-  (* TODO: try to get implicit args working better for these and other such structure? *)
-  
-  Definition maponpaths_id_form {Id : id_form_struct}
-      {Γ} {A A'} (e_A : A = A')
-      {a} {a'} (e_a : a = tm_transportb e_A a')
-      {b} {b'} (e_b : b = tm_transportb e_A b')
-    : Id Γ A a b = Id Γ A' a' b'.
-  Proof.
-    destruct e_A.
-    rewrite tm_transportb_idpath in e_a, e_b.
-    apply maponpaths_12; assumption.
-  Qed.
-
-  Definition id_intro_struct (Id : id_form_struct) : UU
-  := ∑ (refl : forall (Γ : C) (A : C Γ) (a : tm A), tm (Id _ _ a a)),
-       (forall (Γ Γ' : C) (f : Γ' --> Γ) A a,
-         reind_tm f (refl Γ A a)
-         = tm_transportb (id_form_struct_natural _ _ _)
-             (refl _ _ (reind_tm f a))).
-
-  Definition id_intro_struct_pr1 {Id} (refl : id_intro_struct Id) := pr1 refl.
-  Coercion id_intro_struct_pr1 : id_intro_struct >-> Funclass.
-
-  Definition id_intro_struct_natural {Id} (refl : id_intro_struct Id)
-      {Γ Γ'} (f : Γ' --> Γ) A a
-    : reind_tm f (refl Γ A a)
-      = tm_transportb _ (refl Γ' _ _) 
-  := pr2 refl _ _ f A a.
-
-  Definition id_intro_q {Id} (refl : id_intro_struct Id)
-      {Γ Γ'} (f : Γ' --> Γ) A a
-    : f ;; refl Γ A a
-      = refl Γ' _ (reind_tm f a)
-        ;; comp_ext_compare (!id_form_struct_natural _ _ _)
-        ;; q_typecat _ f.
-  Proof.
-    etrans. { apply pathsinv0, reind_tm_q. }
-    apply maponpaths_2. 
-    exact (maponpaths _ (id_intro_struct_natural _ _ _ _)).
-  Qed.
-
-  (** Auxiliary definitions towards the id-elim rule *) 
-
+  Context {C : split_typecat}.
 
   (* TODO: seek elsewhere in library!
    TODO: naming!  order of args isn’t traditional “bind” *)
@@ -133,35 +39,6 @@ Section Identity_types.
     etrans. { eassumption. }
     apply maponpaths; assumption.
   Qed.
-
-  Definition id_based_fam (Id : id_form_struct) {Γ:C} (A : C Γ) (a : tm A)
-    : C (Γ ◂ A)
-  := Id _ _ (reind_tm _ a) (var_typecat A).
-  (* TODO: add defs to make this easier to read/write, e.g. for de Bruijn index variables?? *)
-
-  Definition id_based_fam_natural (Id : id_form_struct)
-    {Γ Γ':C} (f : Γ' --> Γ) (A : C Γ) (a : tm A)
-    : (id_based_fam Id A a) ⦃ q_typecat A f ⦄
-      = id_based_fam Id (A⦃f⦄) (reind_tm f a).
-  Proof.
-    unfold id_based_fam.
-    etrans. { apply id_form_struct_natural. }
-    use maponpaths_id_form.
-    - etrans. { apply pathsinv0, reind_comp_typecat. }
-      etrans. 2: { apply reind_comp_typecat. }
-      apply maponpaths, dpr_q_typecat.
-    - etrans. { apply pathsinv0, reind_compose_tm'. } 
-      etrans. 2: { apply maponpaths, reind_compose_tm'. }
-      etrans. 2: { apply tm_transportf_compose. }
-      etrans.
-      { eapply maponpaths.
-        refine (maponpaths_2_reind_tm _ _). 
-        apply dpr_q_typecat. }
-      etrans. { apply pathsinv0, tm_transportf_compose. }
-      apply tm_transportf_irrelevant.
-    - apply reind_var_typecat_gen.
-  Qed.
-  (* TODO: consistentise direction of [tm_transportf_bind], and generally which side of equation [tm_transportf] goes on? (RHS) *)
 
 
   (* TODO: upstream? and see if can unify with anything? *)
@@ -230,6 +107,135 @@ Section Identity_types.
     apply cxt_map_ext_postcompose_gen.
   Qed.
 
+End Auxiliary.
+
+Section Pi_eta_structure.
+
+  Context {C : split_typecat} (Π : pi_struct C).
+
+  Definition Pi_eta : UU.
+  Proof.
+    refine (forall (Γ : C) (A : C Γ) (B : C (Γ ◂ A)) (p : tm (Π _ A B)),
+               p = _).
+    (* now we have to construct the eta-expansion of [p],
+     which in object-theory pseudocode is [lam x (app p x)] *)
+    refine (pi_intro _ _ _ _ _). (* [pi_intro] is lambda-abstraction *)
+    Fail refine (pi_app _ _ _ _ _ _).
+    (* fails, but informatively:
+       [pi_app] gives a term in a reindexing of B, not B itself.
+    So we need to add a a [transportf].
+    (In the object theory, the type-coercion rule). *)
+    simple refine (transportf _ _ (pi_app Π _ _ _ _ _)).
+    (* In the object theory, the application we want is [ app p x ].
+    But note that this [p] is weakened from [Γ] to [Γ ◂ A],
+    so we need to give that weakening explicitly,
+    i.e. reindexing [p] along the rependent projection from [Γ ◂ A]. *)
+    4: {
+      Fail refine (reind_tm (dpr_typecat A) p).
+    (* informative again: the reindexed pi-type is not definitionally a pi-type *)
+      refine (transportf _ _ (reind_tm (dpr_typecat A) p)).
+      apply pi_form_struct_natural.
+    }
+    (* now supply the argument: the “new variable” / generic term *)
+    2: { apply var_typecat. }
+    (* now some algebra to justify this type-reindexing equality *)
+    eapply pathscomp0. { apply pathsinv0, reind_comp_type_typecat. }
+    eapply pathscomp0. 2: { apply reind_id_type_typecat. }
+    apply maponpaths. 
+    (* look at the definition of the generic term, as a map into a pullback *)
+    unfold var_typecat. apply Auxiliary.Pb_map_commutes_2.
+  Defined.
+
+End Pi_eta_structure.
+
+Section Identity_types.
+  (** The structure to model identity types in a split type-category. *)
+
+  Context (C : split_typecat).
+
+  Definition id_form_struct : UU
+  := ∑ (Id : forall (Γ : C) (A : C Γ) (a b : tm A), C Γ),
+       (forall (Γ Γ' : C) (f : Γ' --> Γ) (A : C Γ) (a b : tm A),
+         (Id Γ A a b) ⦃ f ⦄ = Id Γ' (A⦃f⦄) (reind_tm f a) (reind_tm f b)).
+
+  Definition id_form_struct_pr1 (Id : id_form_struct) := pr1 Id.
+  Coercion id_form_struct_pr1 : id_form_struct >-> Funclass.
+
+  Definition id_form_struct_natural {Id : id_form_struct}
+      {Γ Γ'} (f : Γ' --> Γ) {A} a b
+    : (Id _ _ a b) ⦃ f ⦄ = Id Γ' _ _ _
+  := pr2 Id _ _ f A a b.
+  (* TODO: try to get implicit args working better for these and other such structure? *)
+
+  Definition maponpaths_id_form {Id : id_form_struct}
+      {Γ} {A A'} (e_A : A = A')
+      {a} {a'} (e_a : a = tm_transportb e_A a')
+      {b} {b'} (e_b : b = tm_transportb e_A b')
+    : Id Γ A a b = Id Γ A' a' b'.
+  Proof.
+    destruct e_A.
+    rewrite tm_transportb_idpath in e_a, e_b.
+    apply maponpaths_12; assumption.
+  Qed.
+
+  Definition id_intro_struct (Id : id_form_struct) : UU
+  := ∑ (refl : forall (Γ : C) (A : C Γ) (a : tm A), tm (Id _ _ a a)),
+       (forall (Γ Γ' : C) (f : Γ' --> Γ) A a,
+         reind_tm f (refl Γ A a)
+         = tm_transportb (id_form_struct_natural _ _ _)
+             (refl _ _ (reind_tm f a))).
+
+  Definition id_intro_struct_pr1 {Id} (refl : id_intro_struct Id) := pr1 refl.
+  Coercion id_intro_struct_pr1 : id_intro_struct >-> Funclass.
+
+  Definition id_intro_struct_natural {Id} (refl : id_intro_struct Id)
+      {Γ Γ'} (f : Γ' --> Γ) A a
+    : reind_tm f (refl Γ A a)
+      = tm_transportb _ (refl Γ' _ _) 
+  := pr2 refl _ _ f A a.
+
+  Definition id_intro_q {Id} (refl : id_intro_struct Id)
+      {Γ Γ'} (f : Γ' --> Γ) A a
+    : f ;; refl Γ A a
+      = refl Γ' _ (reind_tm f a)
+        ;; comp_ext_compare (!id_form_struct_natural _ _ _)
+        ;; q_typecat _ f.
+  Proof.
+    etrans. { apply pathsinv0, reind_tm_q. }
+    apply maponpaths_2. 
+    exact (maponpaths _ (id_intro_struct_natural _ _ _ _)).
+  Qed.
+
+  (** Auxiliary definitions towards the id-elim rule *) 
+
+  Definition id_based_fam (Id : id_form_struct) {Γ:C} (A : C Γ) (a : tm A)
+    : C (Γ ◂ A)
+  := Id _ _ (reind_tm _ a) (var_typecat A).
+  (* TODO: add defs to make this easier to read/write, e.g. for de Bruijn index variables?? *)
+
+  Definition id_based_fam_natural (Id : id_form_struct)
+    {Γ Γ':C} (f : Γ' --> Γ) (A : C Γ) (a : tm A)
+    : (id_based_fam Id A a) ⦃ q_typecat A f ⦄
+      = id_based_fam Id (A⦃f⦄) (reind_tm f a).
+  Proof.
+    unfold id_based_fam.
+    etrans. { apply id_form_struct_natural. }
+    use maponpaths_id_form.
+    - etrans. { apply pathsinv0, reind_comp_typecat. }
+      etrans. 2: { apply reind_comp_typecat. }
+      apply maponpaths, dpr_q_typecat.
+    - etrans. { apply pathsinv0, reind_compose_tm'. } 
+      etrans. 2: { apply maponpaths, reind_compose_tm'. }
+      etrans. 2: { apply tm_transportf_compose. }
+      etrans.
+      { eapply maponpaths.
+        refine (maponpaths_2_reind_tm _ _). 
+        apply dpr_q_typecat. }
+      etrans. { apply pathsinv0, tm_transportf_compose. }
+      apply tm_transportf_irrelevant.
+    - apply reind_var_typecat_gen.
+  Qed.
+  (* TODO: consistentise direction of [tm_transportf_bind], and generally which side of equation [tm_transportf] goes on? (RHS) *)
 
   Definition id_refl_map {Id} (refl : id_intro_struct Id)
       {Γ} {A : C Γ} (a : tm A)
@@ -254,8 +260,6 @@ Section Identity_types.
 
   (* TODO: move [tm_transport] to RHS in [maponpaths_2_reind_tm], [reind_id_tm] to fit general convention? 
    Also switch direvtions of [reind_compose_tm], etc? *)
-
-  Require Import TypeTheory.Initiality.SplitTypeCat_Maps.
 
   Definition id_refl_map_natural {Id} (refl : id_intro_struct Id)
       {Γ Γ'} (f : Γ' --> Γ) {A : C Γ} (a : tm A)
