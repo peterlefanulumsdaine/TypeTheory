87fd28c223cf98dcf1c6986bebec61acdeeb75d0
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2024-05-10 17:45:32 +0200

    progress on Glivenko’s theorem, and setting up machinery for proofs

--- TypeTheory/ProofTheory/PropositionalLogic.v
+++ TypeTheory/ProofTheory/PropositionalLogic.v
@@ -104,10 +104,102 @@ Section Expressions.
   6 args: or_elim
  *)
 
+(* generic induction template:
+  induction p as
+    [ Γ i
+    | Γ
+    | Γ φ1 φ2 p1 IH_p1 p2 IH_p2
+    | Γ φ1 φ2 p IP_p
+    | Γ φ1 φ2 p IP_p
+    | Γ φ p IP_p
+    | Γ φ1 φ2 p IP_p
+    | Γ φ1 φ2 p IP_p
+    | Γ φ1 φ2 σ q IH_q p1 IH_p1 p2 IH_p2
+    | Γ φ ψ p IH_p
+    | Γ φ ψ p IH_p q IH_q
+    | Γ φ p IH_p ].
+  - (* assumption *) admit.
+  - (* true_intro *) admit.
+  - (* and_intro *) admit.
+  - (* and_elim_l *) admit.
+  - (* and_elim_r *) admit.
+  - (* false_elim *) admit.
+  - (* or_intro_l *) admit.
+  - (* or_intro_r *) admit.
+  - (* or_elim *) admit.
+  - (* implies_intro *) admit.
+  - (* implies_elim *) admit.
+  - (* raa *) admit.
+*)
+
   (* TODO: find a good general machinery for simplifying defs of matches/fixpoints in which most cases are “parallel”/“obvious”, etc; for instance, [is_raa_free_below] should just need specifying the base case for [raa _] and then saying “in all other cases, take [decidableAnd] of all other subderivations” *)
+
+  Definition assumption' (Γ : context) {φ} (i : Γ) (e : Γ i = φ) : proof Γ φ.
+  Proof.
+    destruct e; apply assumption.
+  Defined.
+
+  Definition assumption_top (Γ : context) {φ} : proof (context_extend Γ φ) φ.
+  Proof.
+    exact (assumption (context_extend _ _) dB_top).
+  Defined.
+
 End Expressions.
 
-(* todo: better to define as [is_raa_free], or as [uses_raa] and then negate? *)
+(* Substitution of derivations into contexts *)
+Section Substitution.
+
+  Definition substitute_proof {Γ} {φ} (p : proof Γ φ) {Δ} (qs : forall i:Γ, proof Δ (Γ i)) : proof Δ φ.  
+  Proof.
+  revert Δ qs.
+  induction p as
+    [ Γ i
+    | Γ
+    | Γ φ1 φ2 p1 IH_p1 p2 IH_p2
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 σ q IH_q p1 IH_p1 p2 IH_p2
+    | Γ φ ψ p IH_p
+    | Γ φ ψ p IH_p q IH_q
+    | Γ φ p IH_p ]; intros Δ qs.
+  - (* assumption *) apply qs.
+  - (* true_intro *) apply true_intro.
+  - (* and_intro *) apply and_intro; [ apply IH_p1 | apply IH_p2 ]; apply qs.
+  - (* and_elim_l *) eapply and_elim_l, IH_p, qs.
+  - (* and_elim_r *) eapply and_elim_r, IH_p, qs.
+  - (* false_elim *) apply false_elim, IH_p, qs.
+  - (* or_intro_l *) apply or_intro_l, IH_p, qs.
+  - (* or_intro_r *) apply or_intro_r, IH_p, qs.
+  - (* or_elim *) eapply or_elim.
+    + apply IH_q, qs.
+    + apply IH_p1. admit. (* TODO: define extension of substitutions (requires renaming/generalised weakening) *)
+    + apply IH_p2. admit.
+  - (* implies_intro *) eapply implies_intro, IH_p. admit.
+  - (* implies_elim *) eapply implies_elim;
+                         [ apply IH_p | apply IH_q ]; apply qs.
+  - (* raa *) eapply raa, IH_p. admit.
+  Admitted.
+
+  Definition substitute_proof_top {Γ} {φ ψ} (p : proof (context_extend Γ ψ) φ) (q : proof Γ ψ) : proof Γ φ.
+  Proof.
+    apply (substitute_proof p).
+    apply dB_Sn_rect.
+    - apply q.
+    - apply assumption.
+  Defined.
+
+  (* TODO: should just use renaming, not substitution, once that’s defined. *)
+  Definition weaken_proof {Γ φ ψ} (p : proof Γ φ) : proof (context_extend Γ ψ) φ.
+  Proof.
+  Admitted.
+
+End Substitution.
+
+(* TODO: better to define as [is_raa_free], or as [uses_raa] and then negate? *)
+(* TODO: better to define as a bool, rather than as a DecidableProposition? *)
 Fixpoint is_raa_free {Γ : context} {φ1 : proposition} (p : proof Γ φ1)
     : DecidableProposition
   := match p with
@@ -125,8 +217,69 @@ Fixpoint is_raa_free {Γ : context} {φ1 : proposition} (p : proof Γ φ1)
     | implies_elim p q => decidableAnd (is_raa_free p) (is_raa_free q)
   end.
 
-Theorem glivenko {Γ} {φ} : proof Γ φ
-    -> ∑ (p : proof (context_extend Γ (not_prop φ)) false_prop), is_raa_free p.
+(* TODO: better to separate the transformation from the proof of RAA-freeness?
+  Almost certainly! *)
+Theorem glivenko {Γ} {φ} (p : proof Γ φ)
+    : ∑ (p' : proof (context_extend Γ (not_prop φ)) false_prop), is_raa_free p'.
+Proof.
+  induction p as
+    [ Γ i
+    | Γ p
+    | Γ φ1 φ2 p1 IH_p1 p2 IH_p2
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 p IH_p
+    | Γ φ1 φ2 q IH_q p1 IH_p1 p2 IH_p2
+    | Γ φ ψ p IH_p
+    | Γ φ ψ p IH_p q IH_q
+    | Γ φ p IH_p ]; use tpair.
+  - (* assumption *)
+    eapply implies_elim.
+    + apply assumption_top.
+    + apply weaken_proof, assumption.
+  - repeat constructor. admit.
+     (* should be automatic once weaken_proof defined, if that computes well;
+     if not, give it by hand instead using assumption', dB_next. *)
+  - (* true_intro *)
+    eapply implies_elim.
+    + use assumption'. { apply dB_top. } apply idpath.
+    + apply true_intro.
+  - repeat constructor.
+  - (* and_intro *)
+    apply (@substitute_proof_top _ _ (not_prop φ2)) .
+    { admit. } (* exchange + weakening of IH_p2 *)
+    apply implies_intro.
+    apply (substitute_proof (pr1 IH_p1)).
+    apply dB_Sn_rect.
+    2: { intros i. 
+       use assumption'. { apply dB_next, dB_next, i. } apply idpath. }
+    apply implies_intro.
+    eapply implies_elim.
+    { use assumption'. { apply dB_next, dB_next, dB_top. } apply idpath. }
+    apply and_intro.
+    + apply assumption_top.
+    + apply weaken_proof, assumption_top.
+  - admit.
+  - (* and_elim_l *) admit.
+  - admit.
+  - (* and_elim_r *) admit.
+  - admit.
+  - (* false_elim *) admit.
+  - admit.
+  - (* or_intro_l *) admit.
+  - admit.
+  - (* or_intro_r *) admit.
+  - admit.
+  - (* or_elim *) admit.
+  - admit.
+  - (* implies_intro *) admit.
+  - admit.
+  - (* implies_elim *) admit.
+  - admit.
+  - (* raa *) admit.
+  - admit.
 Admitted.
 
 Definition is_cut_free {Γ : context} {φ1 : proposition} (p : proof Γ φ1) : hProp.
